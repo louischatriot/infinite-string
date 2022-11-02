d8b1532dffc2c214d9735b9133d5c8f278a4b078
Louis Chatriot <louis.chatriot@gmail.com>
2022-11-02 16:34:55 +0100

    split for one domain only

--- main.py
+++ main.py
@@ -1,11 +1,43 @@
 
 
 
-
+# Length of num is between 2 and 15 so we can never span three domains
 def findPosition(num):
+    N = len(num)
+
+    pos = []
+
+    # s is the size of the first full number (if any)
+    # l the size of the incomplete left hand number
+    # r the size of the incomplete right hand number
+    for s in range(1, N + 1):
+        # Case when we span only one domain
+        for l in range(0, s):
+            for r in range(0, s):
+                full = N - l - r
+                if full % s != 0:
+                    continue
+                pos.append([l] + ([s] * (full // s)) + [r])
+
+
+
+    for p in pos:
+        print(p)
+
+
+
     return 0
 
 
+
+
+findPosition("1234567890")
+
+
+
+
+
+1/0
 
 
 
